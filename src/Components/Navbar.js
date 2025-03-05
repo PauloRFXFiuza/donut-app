67f346850bf7c321aad72cbdcda71038a65397ff
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-05 14:31:04 -0300

    v2.8

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,6 +7,15 @@ import HomeWorkIcon from "@mui/icons-material/HomeWork"
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded"
 import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded"
 import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded"
+import { HiOutlineBars3 } from "react-icons/hi2"
+import Drawer from "@mui/material/Drawer"
+import Box from "@mui/material/Box"
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import ListItemText from "@mui/material/ListItemText"
+
 
 const Navbar = () => {
     const[openMenu, setOpenMenu] = useState(false)
@@ -55,9 +64,34 @@ const Navbar = () => {
 
                 <button className="primary-button">Order Now</button>
             </div>
-            <div className="nav-bar-menu-container">
 
+            <div className="nav-bar-menu-container">
+                <HiOutlineBars3 onClick={() => setOpenMenu(true)}/>
             </div>
+
+            <Drawer
+                open={openMenu}
+                onClose={() => setOpenMenu(false)}
+                anchor="right"
+            >
+                <Box
+                    sx= {{width: 250}}
+                    role= "presentation"
+                    onClick= {() => setOpenMenu(false)}
+                    onKeyDown= {() => setOpenMenu(false)}
+                >
+                    <List>
+                        {menuOptions.map((item)=>(
+                            <ListItem key= {item.text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>{item.icon}</ListItemIcon>
+                                    <ListItemText primary={item.text}/>
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Drawer>
         </nav>
 
     )
