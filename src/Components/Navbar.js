81b0d2e2f026f319b147afe40d5a3367e690e58a
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-05 13:42:54 -0300

    v2.3

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,3 +1,4 @@
+import React from "react"
 import Logo from "../Assets/Logo.svg"
 
 const Navbar = () => {
@@ -5,6 +6,13 @@ const Navbar = () => {
         <nav>
             <div>
                 <img src={Logo} alt="Donut logo" />
+            </div>
+            <div>
+                <a href="">Home</a>
+                <a href="">About Us</a>
+                <a href="">Our Work</a>
+                <a href="">Reviews</a>
+                <a href="">Contact</a>
             </div>
         </nav>
 
