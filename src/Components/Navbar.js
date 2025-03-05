898c1e67e46ded70dd78087304d6815269ee6f42
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-05 14:12:20 -0300

    v2.6

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import Logo from "../Assets/Logo.svg"
 import {BsCart2} from "react-icons/bs"
+import HomeIcon from "@mui/icons-material/Home"
 
 const Navbar = () => {
+    const[openMenu, setOpenMenu] = useState(false)
+    const menuOptions = [
+        {
+            text: "Home",
+            icon: <HomeIcon/>,
+        },
+        {
+            text: "About Us",
+            icon: <InfoIcon/>,
+        },
+        {
+            text: "Our Work",
+            icon: <HomeWorkIcon/>,
+        },
+        {
+            text: "Reviews",
+            icon: <CommentRoundedIcon/>,
+        },
+        {
+            text: "Contact",
+            icon: <PhoneRoundedIcon/>,
+        },
+        {
+            text: "Cart",
+            icon: <ShoppingCartRoundedIcon/>,
+        },
+    ]
+
+
     return(
         <nav>
             <div className="nav-logo-container">
