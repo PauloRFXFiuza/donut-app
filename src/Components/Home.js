93949c481db3382632a043ac3b26bb49299a10e6
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-06 08:30:00 -0300

    v2.11

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import NavBar from './Navbar'
 import BannerBackground from '../Assets/home-banner-background.png'
+import {FiArrowRight} from "react-icons/fi"
 
 const Home = () => {
     return (
@@ -15,10 +16,13 @@ const Home = () => {
                     <h1 className="primary-heading">
                         Your Favorite Donut Delivered to Your Home
                     </h1>
+
                     <h1 className="primary-text">
                         Delicious flavors to sweeten your best moments. 
                         Order now and receive a surprise gift.
                     </h1>
+
+                    <button className="secondary-button">Place your order<FiArrowRight/></button>
                 </div>
             </div>
         </div>
