90a0722484f4978951b4c90aaf9862cae478f69b
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-06 14:45:49 -0300

    v4.11

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -1,6 +1,16 @@
 const Testimonial = () => {
     return (
-        <h1>Testimonial</h1>
+        <div className="work-section-wrapper">
+            <div className="work-section-top">
+                <p className="primary-subheading">Reviews</p>
+                <h1 className="primary-heading">What they say about us</h1>
+                <p className="primary-text">
+                Our customers love the quality and taste of our donuts. Each bite is an explosion of 
+                flavor that brings joy and satisfaction. We are always receiving compliments for the 
+                variety of flavors we offer.
+                </p>
+            </div>
+    </div>
     )
 }
 
