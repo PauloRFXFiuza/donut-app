6533afe3f4bb3a8fac269c8081253958bec110e5
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-06 09:55:23 -0300

    v3.9

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,6 @@
 import AboutBackground from "../Assets/about-background1.jpg"
 import AboutBackgroundImage from "../Assets/about-image.png"
+import { BsFillPlayCircleFill } from "react-icons/bs"
 
 const About = () => {
     return (
@@ -13,7 +14,28 @@ const About = () => {
                 <img src={AboutBackgroundImage} alt="Illustration of a hand holding a donut."/>
             </div>
 
+            <div className="about-section-text-container">
+                <p className="primary-subheading">About Us</p>
+                <h1 className="primary-heading">
+                    It’s Important to Know Who Feeds You
+                </h1>
+                <p className="primary-text">
+                    We are a local bakery dedicated to bringing you the most delicious donuts in town.
+                </p>
+                <p className="primary-text">
+                    Our story began with a passion and a desire to create a space where friends and family 
+                    can gather and enjoy sweet moments together.
+                </p>
+                <div className="about-buttons-container">
+                    <button className="secondary-button">Read More</button>
+                    <button className="watch-video-button">
+                        <BsFillPlayCircleFill /> Watch Our Video
+                    </button>
+                </div>
+            </div>
+
         </div>
+
     );
 }
 
