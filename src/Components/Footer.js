c59fbe2b057ab137f9d6cd2266e774db897e11c9
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-06 15:52:14 -0300

    v4.16

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,8 @@
 import Logo from "../Assets/Logo.svg"
+import {BsTwitter} from "react-icons/bs"
+import {SiLinkedin} from "react-icons/si"
+import {BsYoutube} from "react-icons/bs"
+import {FaFacebookF} from "react-icons/fa"
 
 const Footer = () => {
     return (
@@ -6,6 +10,12 @@ const Footer = () => {
             <div className="footer-section-one">
                 <div className="footer-logo-container">
                     <img src={Logo} alt="Donut Logo"/>
+                </div>
+                <div className="footer-icons">
+                    <BsTwitter />
+                    <SiLinkedin />
+                    <BsYoutube />
+                    <FaFacebookF />
                 </div>
             </div>
         </div>
