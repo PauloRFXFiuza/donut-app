e9c06d34374086e42361c2d721f51f6c66c5d98a
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-06 08:23:57 -0300

    v2.10

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,26 @@
 import NavBar from './Navbar'
+import BannerBackground from '../Assets/home-banner-background.png'
 
 const Home = () => {
     return (
         <div className="home-container">
             <NavBar/>
+
+            <div className="home-banner-container">
+                <div className="home-bannerImage-container">
+                    <img src= {BannerBackground} alt= "Home Container's Background"/>
+                </div>
+
+                <div className="home-text-section">
+                    <h1 className="primary-heading">
+                        Your Favorite Donut Delivered to Your Home
+                    </h1>
+                    <h1 className="primary-text">
+                        Delicious flavors to sweeten your best moments. 
+                        Order now and receive a surprise gift.
+                    </h1>
+                </div>
+            </div>
         </div>
     )
 }
