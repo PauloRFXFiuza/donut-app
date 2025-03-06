42bf208cd8c499281c3977288ec90d72d1c96d19
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-06 09:21:59 -0300

    v3.4

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
                     </h1>
 
                     <button className="secondary-button">
-                        Place your order<FiArrowRight/>
+                        Place your order <FiArrowRight/>
                     </button>
 
                     <p className="primary-text">
