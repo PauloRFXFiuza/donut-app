06df810c18ace6481fb5a868cc8ec5f7b6e2a739
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-06 09:47:48 -0300

    v3.8

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,18 @@
 import AboutBackground from "../Assets/about-background1.jpg"
+import AboutBackgroundImage from "../Assets/about-image.png"
 
 const About = () => {
     return (
         <div className= "about-section-container">
+            
             <div className= "about-background-image-container">
                 <img src={AboutBackground} alt="Stack Donuts Illustration"/>
             </div>
+
+            <div className= "about-section-image-container">
+                <img src={AboutBackgroundImage} alt="Illustration of a hand holding a donut."/>
+            </div>
+
         </div>
     );
 }
