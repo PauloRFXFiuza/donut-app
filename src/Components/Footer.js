c6da63f66d9cdf43440463d616534328c3182603
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-06 16:03:35 -0300

    v4.17

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,6 +17,27 @@ const Footer = () => {
                     <BsYoutube />
                     <FaFacebookF />
                 </div>
+                <div className="footer-section-two">
+                    <div className="footer-section-columns">
+                        <span>Quality</span>
+                        <span>Help</span>
+                        <span>Our Brand</span>
+                        <span>Reviews</span>
+                        <span>Our Work</span>
+                    </div>
+                    <div className="footer-section-columns">
+                        <span>11-332233-1234</span>
+                        <span>hello.donut@ghdmail.com</span>
+                        <span>media.donut@ghdmail.com</span>
+                        <span>contact.donut@ghdmail.com</span>
+                    </div>
+                    <div className="footer-section-columns">
+                        <span>Conditions and Terms</span>
+                        <span>Privacy and Politics</span>
+                    </div>
+                </div>
+                
+
             </div>
         </div>
     )
